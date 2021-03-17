1e9dbf4b0727e7a4fa6ae506777cd930dea8e001
dedi <dedimashuri1996@gmail.com>
2021-03-17 12:25:32 +0700

    talenta

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {styles} from './styles';
 
 import {
   SafeAreaView,
@@ -13,88 +14,14 @@ import {
   TextInput,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-
-const App: () => Node = () => {
-  const  [input, setinput] = useState('');
-
-  // //component didmount di hooks
-  // useEffect(() => {
-    // function yang didalam return adalah component will unmount
-  //   return () => {};
-  // }, []);
-
-
-  // //component didupdate
-  // useEffect(() => {},[]);
-
-
+const App = () => {
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.kotak}>
-        <Text>
-          dedi
-        </Text>
-      </View>
-      <View style={[styles.kotak, styles.kotak1]}>
-        <Text>
-          mashuri
-        </Text>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor= "#0bbd9a" barStyle="light-content"/>
       <View>
-        <Image
-          style={{width:400, height:300, borderRadius: 10,}}
-          source={
-            {
-              uri:'https://d5ibtax54de3q.cloudfront.net/eyJidWNrZXQiOiJraWNrYXZlbnVlLWFzc2V0cyIsImtleSI6InByb2R1Y3RzLzIxMjA4L2QxZmIxMzcwY2M2OTljZmNiODJmNWYwOWFmN2Y0MTBiLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6NDAwfSwid2VicCI6eyJxdWFsaXR5Ijo1MH19fQ=='
-            }
-          }
-        />
-        <TextInput
-          style= {{borderWidth: 1, width: 400}}
-          value={input}
-          onChangeText={text => setinput(text)}
-          placeholder="isi text"
-        />
+        <Text >dedi</Text>
       </View>
-      <View>
-        <Image
-          style={{width:400, height:300, borderRadius: 10,}}
-          source={require
-            (
-                './image/foto.png'
-            )
-          }
-        />
-      </View>
-    </ScrollView>
-    )
-};
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'steelblue'
-    },
-
-    kotak: {
-      backgroundColor: 'yellow',
-      height: 350,
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-
-    kotak1: {
-      backgroundColor: 'pink'
-    }
-  
-});
-
+    </SafeAreaView>
+  )
+}
 export default App;
---
+++ styles/index.js
@@ -0,0 +1,18 @@
+
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'steelblue',
+  },
+  kotak: {
+    backgroundColor: 'yellow',
+    height: 300,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  kotak1: {
+    backgroundColor: 'pink',
+  },
+});
